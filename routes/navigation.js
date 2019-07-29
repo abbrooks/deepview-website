764260f17232cd9b4bc68736cbf9beedcd49d12d
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-07-29 14:36:27 -0700

    comments

--- routes/navigation.js
+++ routes/navigation.js
@@ -1,20 +1,33 @@
 module.exports = router=>{
+
+  // for naving to contact page
   router.get('/contact', (req,res)=>{
     res.render('contact.html');
   });
+
+  // for naving to home page
   router.get('/home', (req,res)=>{
     res.redirect('/');
   });
+
+  // for naving to corporate page
   router.get('/corporate', (req,res)=>{
     res.render('corporate.html');
   });
+
+  // for naving to news page
   router.get('/news', (req,res)=>{
     res.render('news.html');
   });
+
+  // for naving to products page
   router.get('/products', (req,res)=>{
     res.render('products.html');
   });
+
+  // for naving to news_pieces page
   router.get('/news_pieces', (req,res)=>{
     res.render('news_pieces.html');
   });
-}
+  
+} // end of module exports
