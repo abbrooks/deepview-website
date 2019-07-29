b2d3fd7b15270295c9b214e51ca80d0bc19f1096
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-07-29 14:37:39 -0700

    text

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 /*************************************************************************
  *
- * BANDA CONFIDENTIAL
+ * Bothe Confidential
  * __________________
  *
  *  Copyright (C) 2019
- *  Banda Incorporated
+ *  Alexander Bothe
  *  All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
- * the property of Banda Incorporated and its suppliers,
+ * the property of Alexander Bothe and his suppliers,
  * if any.  The intellectual and technical concepts contained
  * herein are proprietary to Banda Incorporated
  * and its suppliers and may be covered by U.S. and Foreign Patents,
  * patents in process, and are protected by trade secret or copyright law.
  * Dissemination of this information or reproduction of this material
  * is strictly forbidden unless prior written permission is obtained
- * from Banda Incorporated.
+ * from Alexander Bothe.
  *
 *************************************************************************/
 
