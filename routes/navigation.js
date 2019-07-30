d679dccf805d4daae921df1d95f0c52c54657aed
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-07-29 17:39:07 -0700

    products in a linked from product page

--- routes/navigation.js
+++ routes/navigation.js
@@ -29,5 +29,21 @@ module.exports = router=>{
   router.get('/news_pieces', (req,res)=>{
     res.render('news_pieces.html');
   });
-  
+
+  // for naving to news_pieces page
+  router.get('/imageGuard', (req,res)=>{
+    res.render('image_guard.html');
+  });
+  // for naving to coproate retention page
+  router.get('/retention', (req,res)=>{
+    res.render('corporate_retention.html');
+  });
+  // for naving to chat guard page
+  router.get('/chat_guard', (req,res)=>{
+    res.render('chat_guard.html');
+  });
+  // for naving to eLearning page
+  router.get('/eLearning', (req,res)=>{
+    res.render('eLearning.html');
+  });
 } // end of module exports
