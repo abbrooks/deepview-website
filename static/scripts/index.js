130a8a5b56c058870af052100bd44db39ed64df3
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-07-31 00:22:01 -0700

    more nav stuff

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,7 +4,34 @@ $(function () {
     $nav.toggleClass('scrolled-navbar', $(this).scrollTop() > 1);
   });
 });
-
+/*  router.get('/imageGuard', (req,res)=>{
+    res.render('image_guard.html');
+  });
+  // for naving to coproate retention page
+  router.get('/retention', (req,res)=>{
+    res.render('corporate_retention.html');
+  });
+  // for naving to chat guard page
+  router.get('/chat_guard', (req,res)=>{
+    res.render('chat_guard.html');
+  });
+  // for naving to eLearning page
+  router.get('/eLearning', (req,res)=>{
+    res.render('eLearning.html');
+  });
+} // end of m*/
 function contactUs(){
   window.location='/contact';
+}
+
+function goImageGuard(){
+  window.location = '/imageGuard';
+}
+
+function goChatGuard(){
+  window.location = '/chat_guard';
+}
+
+function goCorpGuard(){
+  window.location = '/retention';
 }
