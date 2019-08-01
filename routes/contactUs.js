20bd52f1d8919cc40271bb84541d4b203795f026
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-08-01 15:41:59 -0700

    contact us

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,28 +1,52 @@
 module.exports = router=>{
-
+const OUR_ADDRESS = 'banda.confirmation@gmail.com'
+const DEEP_VIEW_ADDRESS = 'alexanderrossbothe@gmail.com';
   router.post('/requestADemo', (req,res)=>{
 
   })
 
   router.post('/contactUs', (req,res)=>{
-
+    if (!req.body){
+      console.log('No body sent in contact us.')
+      res.status(200).send('Sorry, it seems somethign went wrong on our end.').end();
+    }
+    else{
+      var {body, firstName, lastName, purpose, email, phone} = req.body;
+      if (!body || !firstName || !lastName || !purpose){
+        console.log('Missing fields in contact us');
+        res.status(200).send('Hmmm...it seems you did not fill out all of the fiels. Please try again. Thank you.').end();
+      }
+      else{
+        var message = firstName + lastName + ' has contacted us from the DeepView website.\nTheir purpose is: ' + purpose + '. Here is what they said.\n\n'+body'\n\nContact Info:\nPhone: ' + phone + '\nEmail: ' + email;
+        var subj = 'From DeepView.com: ' + firstName+' '+lastName+'wants to ' + purpose
+        sendEmail(message, subj, cb=>{
+          if (cb){
+            console.log('There was an error sending mail: ' + cb);
+            res.status(200).send('Hmmm...somethign went wrong on our end. Please refresh this page and try again.')
+          }
+          else{
+            res.status(200).send('We have sent your mail to our customer support team! Thank you.');
+          }
+        })
+      }
+    }
   })
 
-  function sendEmail(body, subject, to, cb){
+  function sendEmail(body, subject1, cb){
     let transporter = nodeMailer.createTransport({
          host: 'smtp.gmail.com', // go daddy email host port
          port: 465, // could be 993
          secure: true,
          auth: {
-             user: 'banda.confirmation@gmail.com',
+             user: OUR_ADDRESS,
              pass: 'N5gdakxq9!'
          }
      });
      mailOptions = {
             from: OUR_ADDRESS, // our address
-            to: address, // who we sending to
-            subject: "Confirmation Code From Banda For "+theGig.name+"", // Subject line
-            text: "Hello, "+req.session.key+". Here is your confirmation code for the event "+theGig.name+"      -----                 "+code+"                 -----               Give this code to the arist at the time of the event in person. They should submit this code on their home page. Also be sure to submit the code the artist gives you on your home page. DO NOT SEND THIS CODE TO ANYONE. You should exchange codes in person at the time of the event. We do this to ensure fair transactions between our customers. Once both codes have been sent we will securly charge you and transfer the artist "+theGig.price+ " for you. If you have any questions at all simply reply to this email. Enjoy the music and thank you for using Banda. —Your team at Banda.", // plain text body
+            to: DEEP_VIEW_ADDRESS, // who we sending to
+            subject: subject1, // Subject line
+            text: body,
             html: '' // html body
         };
         transporter.sendMail(mailOptions, (error, info) => {
