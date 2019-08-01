109301d1899ff06e6a3dfc27e01fa560d04e8034
abtheteabandit <bradleybrooks@wustl.edu>
2019-08-01 09:35:43 -0500

    index page fully responsive.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,7 +4,16 @@ $(function () {
     var $navDrop = $("#navbar-dropdown");
     $nav.toggleClass('scrolled-navbar', $(this).scrollTop() > 1);
     $navDrop.toggleClass('scrolled-navbar-dropdown', $(this).scrollTop() > 1);
+  });
+  $(document).scroll(function () {
+    if($(this).scrollTop() == 0 && mobileNavOpen){
+      // var $nav = $("#navbar-container");
+      // var $navDrop = $("#navbar-dropdown");
+      // $nav.toggleClass('scrolled-navbar');
+      // $navDrop.toggleClass('scrolled-navbar-dropdown');
+    }else{
 
+    }
   });
 });
 /*  router.get('/imageGuard', (req,res)=>{
@@ -31,10 +40,18 @@ var mobileNavOpen = false;
 
 function toggleNavDropdown(){
   var drop = document.getElementById('navbar-dropdown');
+  var nav = document.getElementById('navbar-container');
+
   if(mobileNavOpen){
     drop.style.display = "none";
     mobileNavOpen = false;
   }else{
+    if(!(drop.classList.contains('scrolled-navbar-dropdown'))){
+      drop.classList.toggle('scrolled-navbar-dropdown');
+    }
+    if(!(nav.classList.contains('scrolled-navbar'))){
+      nav.classList.toggle('scrolled-navbar');
+    }
     drop.style.display = "block";
     mobileNavOpen = true;
   }
