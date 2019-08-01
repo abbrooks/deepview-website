5365315f50c0a641c0136da80b76491bd0cbd71b
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-08-01 15:29:27 -0700

    can create articles from admin portal

---
+++ static/scripts/admin.js
@@ -0,0 +1,62 @@
+console.log('Loaded admin script')
+document.getElementById("create_article_modal").hidden=true;
+cats=[]
+function addCat(){
+  console.log('in add cat')
+  cat = document.getElementById('category').value;
+  if (cat==null || cat=='' || cat==' '){
+    alert('Sorry, you must put a no empty value in to add a category.')
+    return
+  }
+  cats.push(cat);
+  document.getElementById('category').value=null
+  document.getElementById('category').innerHTML=''
+  return
+}
+
+
+function postArticle(){
+  var title = document.getElementById('title').value;
+  var body = document.getElementById('body').value;
+  var creator = document.getElementById('creator').value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if (title=='' || title==' ' || title==null){
+    alert('Sorry, you must give your article a title.');
+    return
+  }
+  if (body=='' || body==' ' || body==null){
+    alert('Sorry, you must give your article a body.');
+    return
+  }
+  if (creator=='' || creator==' ' || creator==null){
+    alert('Sorry, you must give your article a creator.');
+    return
+  }
+  if (cats.length<=0){
+    alert('Sorry, you must give your article at least one category.');
+    return
+  }
+  if (username=='' || username==' ' || username==null){
+    alert('Sorry, to post an article you must proivde a valid admin username.');
+    return
+  }
+  if (password=='' || password==' ' || password==null){
+    alert('Sorry, to post an article you must proivde a valid admin password.');
+    return
+  }
+
+  $.post('/newsPiece', {'title':title, 'password':password, 'username':username, 'creator':creator, 'body':body, 'categories':cats}, res=>{
+    if (res){
+      alert(res)
+    }
+    else{
+      alert('Hmmm...something went wrong please refresh and try again.')
+    }
+  });
+}
+
+function openArticle(){
+  document.getElementById("create_article_modal").hidden=false;
+
+}
