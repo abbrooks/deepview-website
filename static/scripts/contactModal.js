596f2f0f050c674d341741f0ba6051e53a204c7a
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-08-11 16:52:48 -0700

    more cotnact us stuff

--- static/scripts/contactModal.js
+++ static/scripts/contactModal.js
@@ -17,3 +17,36 @@ function clearContactForm(){
 function prepareContactModal(){
   document.getElementById('modal-wrapper-contact').style.display = 'block';
 }
+
+function submitContactForm(){
+  var name = document.getElementById('contact-name').value;
+  var email = document.getElementById('contact-email').value;
+  var phone = document.getElementById('contact-phone').value;
+  var purpose = document.getElementById('contact-purpose').value;
+  var message = document.getElementById('contact-textarea').value;
+
+  if (name=='' || name == ' ' || name==null){
+    alert('Sorry, you must provide your name.')
+    return
+  }
+  if (email=='' || email == ' ' || email==null){
+    alert('Sorry, you must proivde your email.')
+    return
+  }
+  if (phone=='' || phone == ' ' || phone==null){
+    alert('Sorry, you must provide your phone.')
+    return
+  }
+  if (purpose=='' || purpose == ' ' || purpose==null){
+    alert('Sorry, you must provide your purpose.')
+    return
+  }
+  if (message=='' || message == ' ' || message==null){
+    alert('Sorry, you must enter your message.')
+    return
+  }
+  //body, firstName, lastName, purpose, email, phone
+  $.post('/contactUs', {"body":message, 'purpose':purpose, 'phone':phone,'email':email, 'name':name}, res=>{
+    alert(res)
+  })
+}
